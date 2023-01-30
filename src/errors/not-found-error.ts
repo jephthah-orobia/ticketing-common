0b94b860b21cb6722c295042c9a4a6ae3e163c99
Jephthah Orobia <jephthah.orobia@r5-1.deped.gov.ph>
2023-01-31 04:14:41 +0800

    change NotFoundError code to 404

--- src/errors/not-found-error.ts
+++ src/errors/not-found-error.ts
@@ -1,7 +1,7 @@
 import { SerializableError } from './serializable-error';
 
 export class NotFoundError extends SerializableError {
-  statusCode: number = 400;
+  statusCode: number = 404;
   serializeErrors(): { message: string; field?: string | undefined }[] {
     return [{ message: 'Not Found' }];
   }
