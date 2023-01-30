4418f44cf257e04d724574b1c177fb8ae580af63
Jephthah Orobia <jephthah.orobia@r5-1.deped.gov.ph>
2023-01-30 13:41:00 +0800

    Fix wrong import 'NotFoundError'

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ export * from './errors/serializable-error';
 export * from './errors/bad-request-error';
 export * from './errors/database-validation-error';
 export * from './errors/not-authorized-error';
-export * from './errors/not-authorized-error';
+export * from './errors/not-found-error';
 export * from './errors/request-validation-error';
 
 export * from './middlewares/current-user';
