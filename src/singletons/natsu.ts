2906ebb98c83dc6d05c479068736fddc5b2e3fd4
Jephthah Orobia <jephthah.orobia@r5-1.deped.gov.ph>
2023-02-06 01:59:56 +0800

    Added new singleton: natsu

---
+++ src/singletons/natsu.ts
@@ -0,0 +1,27 @@
+import nats, { Stan } from 'node-nats-streaming';
+
+class Natsu {
+  private _client?: Stan;
+
+  get client() {
+    if (!this._client) {
+      throw new Error(
+        'Make a connection first by using .connect(clusterId, clientId, url)'
+      );
+    }
+    return this._client;
+  }
+
+  connect(cid: string, clientId: string, url: string): Promise<Stan> {
+    this._client = nats.connect(cid, clientId, { url });
+
+    return new Promise((resolve, reject) => {
+      this.client.on('connect', () => resolve(this.client));
+      this.client.on('error', (err) => reject(err));
+    });
+  }
+}
+
+const natsu = new Natsu();
+
+export default { natsu };
