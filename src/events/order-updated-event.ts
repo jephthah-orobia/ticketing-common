d828fc36866ddd35662be35c442dcfec1edb9e53
Jephthah Orobia <jephthah.orobia@r5-1.deped.gov.ph>
2023-02-09 11:55:15 +0800

    include __v

---
+++ src/events/order-updated-event.ts
@@ -0,0 +1,17 @@
+import { Subjects } from '../enums/subjects';
+import { OrderStatus } from '../enums/order-status';
+
+export interface OrderUpdatedEvent {
+  subject: Subjects.TicketUpdated;
+  data: {
+    id: string;
+    status: OrderStatus;
+    userId: string;
+    expiresAt: string;
+    ticket: {
+      id: string;
+      price: number;
+    };
+    __v: number;
+  };
+}
