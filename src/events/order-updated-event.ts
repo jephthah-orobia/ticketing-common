43e6e9c9748ab7a7aa7c2bd8b8296e964af4d0d7
Jephthah Orobia <jephthah.orobia@r5-1.deped.gov.ph>
2023-02-11 03:46:43 +0800

    fix bug: wrong SubjectType for OrderUpdatedEvent

--- src/events/order-updated-event.ts
+++ src/events/order-updated-event.ts
@@ -2,16 +2,12 @@ import { Subjects } from '../enums/subjects';
 import { OrderStatus } from '../enums/order-status';
 
 export interface OrderUpdatedEvent {
-  subject: Subjects.TicketUpdated;
+  subject: Subjects.OrderUpdated;
   data: {
     id: string;
     status?: OrderStatus;
     userId?: string;
     expiresAt?: string;
-    ticket?: {
-      id: string;
-      __v: number;
-    };
     __v: number;
   };
 }
